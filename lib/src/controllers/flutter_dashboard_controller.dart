f240ce8892654762c809df8e49972d1c3d694413
Arjun <philicstream@gmail.com>
2022-05-06 23:57:09 +0530

    controller updated

--- lib/src/controllers/flutter_dashboard_controller.dart
+++ lib/src/controllers/flutter_dashboard_controller.dart
@@ -1,9 +1,7 @@
 part of './controllers.dart';
 
-class FlutterDashboardController extends GetxController
-    with GetSingleTickerProviderStateMixin {
-  static FlutterDashboardController get to =>
-      Get.find<FlutterDashboardController>();
+class FlutterDashboardController extends GetxController with GetSingleTickerProviderStateMixin {
+  static FlutterDashboardController get to => Get.put(FlutterDashboardController());
 
   final FlutterDashboardNavService _navService = FlutterDashboardNavService.to;
 
@@ -11,20 +9,16 @@ class FlutterDashboardController extends GetxController
 
   final RxString currentRoute = ''.obs, currentPageTitle = ''.obs;
 
-  final RxBool isScreenLoading = false.obs,
-      isDrawerOpen = false.obs,
-      isTileExpantionChanged = false.obs;
+  final RxBool isScreenLoading = false.obs, isDrawerOpen = false.obs, isTileExpantionChanged = false.obs;
 
   late AnimationController expansionController;
   late FocusNode focusNode;
 
   final GlobalKey<ScaffoldState> drawerKey = GlobalKey<ScaffoldState>();
 
-  String dashboardInitialRoute =
-      "${DashboardRoutes.DASHBOARD}${FlutterDashboardMaterialApp.of(Get.context!).dashboardItems[0].page!.name}";
+  String dashboardInitialRoute = "${DashboardRoutes.DASHBOARD}${FlutterDashboardMaterialApp.of(Get.context!).dashboardItems[0].page!.name}";
 
-  final RxMap<String, FlutterDashboardItem> finalRoutes =
-      RxMap<String, FlutterDashboardItem>();
+  final RxMap<String, FlutterDashboardItem> finalRoutes = RxMap<String, FlutterDashboardItem>();
 
   FlutterDashboardItem get selectedDrawerItem {
     if (finalRoutes.containsKey(currentPageTitle.value)) {
@@ -44,12 +38,24 @@ class FlutterDashboardController extends GetxController
     }
   }
 
+  final List<GetPage> _allPagesWithChildrens = [];
+
+  void _addChildrenRoutes(List<GetPage> pages) {
+    for (var _page in pages) {
+      if (_page.children.isNotEmpty) {
+        if (!_allPagesWithChildrens.contains(_page)) {
+          _allPagesWithChildrens.add(_page);
+        }
+
+        _addChildrenRoutes(_page.children);
+      } else {
+        _allPagesWithChildrens.add(_page);
+      }
+    }
+  }
+
   List<FlutterDashboardItem> expandItems(List<FlutterDashboardItem> items) {
-    return items
-        .expand((FlutterDashboardItem element) => element.subItems.isEmpty
-            ? [element]
-            : expandItems(element.subItems))
-        .toList();
+    return items.expand((FlutterDashboardItem element) => element.subItems.isEmpty ? [element] : expandItems(element.subItems)).toList();
   }
 
   @override
@@ -58,6 +64,8 @@ class FlutterDashboardController extends GetxController
       // print(_navService.finalRoutes);
       _addRoutes(_navService.rawFooterRoutes);
       _addRoutes(_navService.finalRoutes);
+      _addChildrenRoutes(_navService.finalRoutes.map((element) => element.page!).toList());
+      _addChildrenRoutes(_navService.rawFooterRoutes.map((element) => element.page!).toList());
     });
 
     isScreenLoading(true);
@@ -69,12 +77,100 @@ class FlutterDashboardController extends GetxController
       value: 1.0,
     );
 
-    ever(currentRoute, (String location) {
-      String _location = location.split('/').last.toLowerCase();
+    // ever(currentRoute, (String location) {
+    //   String _location = location.split('/').last.toLowerCase();
 
-      if (_location.isEmpty || _location == 'dashboard') {
+    //   if (_location.isEmpty || _location == 'dashboard') {
+    //     currentPageTitle(_navService.rawRoutes.first.title);
+    //   } else {
+    //     // print(_location);
+    //     // print(finalRoutes);
+    //     // print(finalRoutes);
+    //     // print(finalRoutes.entries.map((element) =>
+    //     //     _location == element.value.page?.name.split('/').last));
+
+    //     // print(finalRoutes.values.where((FlutterDashboardItem element) =>
+    //     //     _location.contains('${element.page?.name.split('/').last}')));
+
+    //     WidgetsBinding.instance?.addPersistentFrameCallback((_) {
+    //       if (finalRoutes.values.where((FlutterDashboardItem element) => _location.contains('${element.page?.name.split('/').last}')).isNotEmpty) {
+    //         // if (finalRoutes.entries
+    //         //     .where((element) =>
+    //         //         _location == element.value.page?.name.split('/').last)
+    //         //     .isNotEmpty) {
+    //         //   currentPageTitle(finalRoutes.entries
+    //         //       .singleWhere((element) =>
+    //         //           _location == element.value.page?.name.split('/').last)
+    //         //       .key);
+    //         // } else {
+    //         //   currentPageTitle("404");
+    //         //   delegate?.toNamed(DashboardRoutes.ERROR404);
+    //         // }
+    //         currentPageTitle(
+    //             finalRoutes.values.singleWhere((FlutterDashboardItem element) => _location.contains('${element.page?.name.split('/').last}')).title);
+    //       } else {
+    //         currentPageTitle("404");
+    //         delegate?.toNamed(DashboardRoutes.ERROR404);
+    //       }
+    //     });
+
+    //     // Get.log('Current dashboard route : ${currentPageTitle.value}');
+    //   }
+    // });
+    ever(currentRoute, (String location) {
+      // String _location = location.split('/').last.toLowerCase();
+
+      if (location.isEmpty || location.split('/').last == 'dashboard' || location == '/' || location == '/index' || location == '/index.html') {
         currentPageTitle(_navService.rawRoutes.first.title);
       } else {
+        WidgetsBinding.instance?.addPostFrameCallback((_) {
+          _allPagesWithChildrens.map((e) {
+            return location.startsWith(DashboardRoutes.DASHBOARD + e.name);
+          }).toList();
+          // print(_allPagesWithChildrens.firstWhere((element) =>
+          //     location.contains(DashboardRoutes.DASHBOARD + element.name)));
+          if (_allPagesWithChildrens.where((element) => location.startsWith(DashboardRoutes.DASHBOARD + element.name)).isNotEmpty) {
+            if (_allPagesWithChildrens
+                .firstWhere((element) => location.startsWith(DashboardRoutes.DASHBOARD + element.name))
+                .name
+                .contains(location.split('/').last)) {
+              currentPageTitle(finalRoutes.entries
+                  .toList()
+                  .firstWhere((element) =>
+                      element.value.page!.name ==
+                      _allPagesWithChildrens.firstWhere((element) => location.startsWith(DashboardRoutes.DASHBOARD + element.name)).name)
+                  .value
+                  .title);
+            } else {
+              // Need to check later
+              if (_allPagesWithChildrens.any((element) => element.name == '/${location.split('/').last}')) {
+                currentPageTitle(finalRoutes.entries
+                    .toList()
+                    .firstWhere((element) =>
+                        element.value.page!.name ==
+                        _allPagesWithChildrens.firstWhere((element) => location.startsWith(DashboardRoutes.DASHBOARD + element.name)).name)
+                    .value
+                    .title);
+              } else {
+                currentPageTitle("404");
+                delegate?.toNamed(DashboardRoutes.ERROR404);
+              }
+            }
+          } else {
+            currentPageTitle("404");
+            delegate?.toNamed(DashboardRoutes.ERROR404);
+          }
+
+          Get.log('Current dashboard route : ${currentPageTitle.value}');
+        });
+
+        // _location.startsWith(pattern);
+        // WidgetsBinding.instance?.addPostFrameCallback((_) {
+        // for (var item in finalRoutes.entries.toList()) {
+        //   print(item.value.page!.name);
+        // }
+
+        // });
         // print(_location);
         // print(finalRoutes);
         // print(finalRoutes);
@@ -84,36 +180,36 @@ class FlutterDashboardController extends GetxController
         // print(finalRoutes.values.where((FlutterDashboardItem element) =>
         //     _location.contains('${element.page?.name.split('/').last}')));
 
-        WidgetsBinding.instance?.addPersistentFrameCallback((_) {
-          if (finalRoutes.values
-              .where((FlutterDashboardItem element) =>
-                  _location.contains('${element.page?.name.split('/').last}'))
-              .isNotEmpty) {
-            // if (finalRoutes.entries
-            //     .where((element) =>
-            //         _location == element.value.page?.name.split('/').last)
-            //     .isNotEmpty) {
-            //   currentPageTitle(finalRoutes.entries
-            //       .singleWhere((element) =>
-            //           _location == element.value.page?.name.split('/').last)
-            //       .key);
-            // } else {
-            //   currentPageTitle("404");
-            //   delegate?.toNamed(DashboardRoutes.ERROR404);
-            // }
-            currentPageTitle(finalRoutes.values
-                .singleWhere((FlutterDashboardItem element) =>
-                    _location.contains('${element.page?.name.split('/').last}'))
-                .title);
-          } else {
-            currentPageTitle("404");
-            delegate?.toNamed(DashboardRoutes.ERROR404);
-          }
-        });
+        // WidgetsBinding.instance?.addPostFrameCallback((_) {
+        //   if (finalRoutes.values
+        //       .where((FlutterDashboardItem element) =>
+        //           _location.startsWith('${element.page?.name.split('/').last}'))
+        //       .isNotEmpty) {
+        // if (finalRoutes.entries
+        //     .where((element) =>
+        //         _location == element.value.page?.name.split('/').last)
+        //     .isNotEmpty) {
+        //   currentPageTitle(finalRoutes.entries
+        //       .singleWhere((element) =>
+        //           _location == element.value.page?.name.split('/').last)
+        //       .key);
+        // } else {
+        //   currentPageTitle("404");
+        //   delegate?.toNamed(DashboardRoutes.ERROR404);
+        // }
+        // currentPageTitle(finalRoutes.values
+        //     .singleWhere((FlutterDashboardItem element) => _location
+        //         .startsWith('${element.page?.name.split('/').last}'))
+        //     .title);
+        //   } else {
+        //     currentPageTitle("404");
+        //     delegate?.toNamed(DashboardRoutes.ERROR404);
+        //   }
+        // });
 
-        // Get.log('Current dashboard route : ${currentPageTitle.value}');
       }
     });
+
     super.onInit();
   }
 
